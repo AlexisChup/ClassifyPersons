fb9ac34c642c78bf2da02b6283a9dc858b7373d4
Alexis Chupin <47422165+AlexisChup@users.noreply.github.com>
2021-04-29 15:58:16 +0200

    Layout is Done. Need to Refactor file

--- src/ClassifyPersons/ClassifyPersons.tsx
+++ src/ClassifyPersons/ClassifyPersons.tsx
@@ -1,3 +1,5 @@
+import { Button } from "reactstrap";
+
 const cssClassifyPersons = {
   display: "flex",
   flexDirection: "column",
@@ -5,21 +7,85 @@ const cssClassifyPersons = {
   height: "100%",
   width: "100%"
 };
-const cssHalfPart = { display: "flex", flexGrow: 1 };
+const cssHalfPart = {
+  display: "flex",
+  flexGrow: 1,
+  margin: 5,
+  backgroundColor: "#CCC",
+  padding: 10
+};
+
+// CSS PERSONSGROUP
+const cssPersonsGroup = {
+  backgroundColor: "green",
+  width: "200px",
+  marginRight: 5,
+  display: "flex",
+  flexDirection: "column"
+};
+
+const cssPersonsGroupHeader = {
+  backgroundColor: "red",
+  color: "white"
+};
+
+const cssPersonsGroupBody = {
+  backgroundColor: "blue",
+  display: "flex",
+  flexGrow: 1
+};
+
+const cssPersonsGroupFooter = {
+  backgroundColor: "purple",
+  color: "white"
+};
+
+// CSS UNCLASSIFIEDPERSONS
+const cssAddGroupPersons = {
+  backgroundColor: "green",
+  width: "200px",
+  marginRight: 5
+};
+
+const cssUnclassifiedPersons = {
+  display: "flex",
+  flexGrow: 1,
+  backgroundColor: "salmon"
+};
 
 const ClassifyPersons = () => {
   return (
     <div style={cssClassifyPersons}>
-      <div style={cssHalfPart}>PersonsGroup</div>
-      <div style={cssHalfPart}>UnclassifiedPersons</div>
+      {/* PersonsGroup */}
+      <div style={cssHalfPart}>
+        <div style={cssPersonsGroup}>
+          <div style={cssPersonsGroupHeader}>Header</div>
+          <div style={cssPersonsGroupBody}>Body</div>
+          <div style={cssPersonsGroupFooter}>Footer</div>
+        </div>
+      </div>
+      {/* UnclassifiedPersons */}
+      <div style={cssHalfPart}>
+        {/* ADD GROUP */}
+        <div style={cssAddGroupPersons}></div>
+        <div style={cssUnclassifiedPersons}></div>
+      </div>
+      {/* FOOTER */}
       <div
         style={{
           display: "flex",
           justifyContent: "flex-end",
-          alignItems: "stretch"
+          alignItems: "stretch",
+          marginRight: 5,
+          marginBottom: 5
         }}
       >
-        <div>Footer</div>
+        <Button color="primary" className="mr-1">
+          Save
+        </Button>
+        <Button color="danger" className="ml-1">
+          Reset
+        </Button>
       </div>
     </div>
   );
